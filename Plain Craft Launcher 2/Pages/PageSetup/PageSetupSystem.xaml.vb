5cef4cc48b22c63e05c0183ce8f2ad747931b777
Camille <zhlytc@126.com>
2026-09-18 14:02:18 +0800

    复选框改为按钮

--- Plain Craft Launcher 2/Pages/PageSetup/PageSetupSystem.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageSetup/PageSetupSystem.xaml.vb
@@ -107,6 +107,7 @@ Public Class PageSetupSystem
         UpdateCheckByButton()
     End Sub
     '重置提示状态
+<<<<<<< Updated upstream
     Private Sub CheckSystemHintReset_Change(sender As Object, user As Boolean) Handles CheckSystemHintReset.Change
         If AniControlEnabled <> 0 Then Return
         If Not user Then Return
@@ -127,6 +128,24 @@ Public Class PageSetupSystem
             Return
         End If
         Settings.GetHintsToReset("Hint").ForEach(Sub(k) Settings.Reset(k))
+=======
+    Private Sub BtnSystemHintReset_Click(sender As Object, e As EventArgs) Handles BtnSystemHintReset.Click
+        Dim HintKeys = Settings.GetHintsToReset("Hint")
+        If HintKeys.Count = 0 Then
+            Hint("没有提示被隐藏！", Log:=False)
+            Return
+        End If
+        If HasDownloadingTask() Then
+            Hint("请在所有下载任务完成后再来重置提示！", Log:=False)
+            Return
+        End If
+        If McLaunchLoader.State = LoadState.Loading Then
+            Hint("请在当前启动任务结束后再来重置提示！", Log:=False)
+            Return
+        End If
+        If MyMsgBox("是否要重置提示状态？该操作会重启 PCL 且不可撤销。", "重置确认", "确定并重启", "取消", IsWarn:=True) <> 1 Then Return
+        HintKeys.ForEach(Sub(k) Settings.Reset(k))
+>>>>>>> Stashed changes
         StartProcess(PathExe, "--wait")
         FrmMain.EndProgram(False)
     End Sub
