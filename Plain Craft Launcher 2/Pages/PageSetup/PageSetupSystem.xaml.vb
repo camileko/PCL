0f9f3c81c2dae956c2003862334945a840443446
Camille <zhlytc@126.com>
2026-09-18 14:30:20 +0800

    移除

--- Plain Craft Launcher 2/Pages/PageSetup/PageSetupSystem.xaml.vb
+++ Plain Craft Launcher 2/Pages/PageSetup/PageSetupSystem.xaml.vb
@@ -107,28 +107,6 @@ Public Class PageSetupSystem
         UpdateCheckByButton()
     End Sub
     '重置提示状态
-<<<<<<< Updated upstream
-    Private Sub CheckSystemHintReset_Change(sender As Object, user As Boolean) Handles CheckSystemHintReset.Change
-        If AniControlEnabled <> 0 Then Return
-        If Not user Then Return
-        If Not sender.Checked Then
-            sender.SetChecked(True, False)
-            Hint("没有提示被手动关闭！", Log:=False)
-            Return
-        End If
-        Dim BlockHint = If(HasDownloadingTask(), "请在所有下载任务完成后再来重置提示状态！",
-                         If(McLaunchLoader.State = LoadState.Loading, "请在当前启动完成结束后再来重置提示状态！", Nothing))
-        If BlockHint IsNot Nothing Then
-            sender.SetChecked(False, False)
-            Hint(BlockHint, Log:=False)
-            Return
-        End If
-        If MyMsgBox("是否要重置提示状态？该操作会重启 PCL 且不可撤销。", "重置确认", "确定并重启", "取消", IsWarn:=True) <> 1 Then
-            sender.SetChecked(False, False)
-            Return
-        End If
-        Settings.GetHintsToReset("Hint").ForEach(Sub(k) Settings.Reset(k))
-=======
     Private Sub BtnSystemHintReset_Click(sender As Object, e As EventArgs) Handles BtnSystemHintReset.Click
         Dim HintKeys = Settings.GetHintsToReset("Hint")
         If HintKeys.Count = 0 Then
@@ -145,7 +123,6 @@ Public Class PageSetupSystem
         End If
         If MyMsgBox("是否要重置提示状态？该操作会重启 PCL 且不可撤销。", "重置确认", "确定并重启", "取消", IsWarn:=True) <> 1 Then Return
         HintKeys.ForEach(Sub(k) Settings.Reset(k))
->>>>>>> Stashed changes
         StartProcess(PathExe, "--wait")
         FrmMain.EndProgram(False)
     End Sub
